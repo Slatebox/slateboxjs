01eed43b097ee33314da9fee757126ec3eaba9ca
Tim Heckel <tim@slatebox.com>
2022-03-06 11:28:21 -0600

    Configured eslint, prettier, and npm workflow

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
