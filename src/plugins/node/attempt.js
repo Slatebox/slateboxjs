4e0a1f0fca58fc45bb968c09b7acdf08793be5ab
Tim Heckel <tim@slatebox.com>
2025-05-31 22:22:52 +0000

    Latest

--- src/plugins/node/attempt.js
+++ src/plugins/node/attempt.js
@@ -1,11 +1,11 @@
-import { Slatebox } from '..././index
+import { Slatebox } from '..././index';
 
 Slatebox.base.registerPlugin({
-    name: "attempt"
-  , opts: {}
-  , plugin: class extends Slatebox.node {
+  name: 'attempt',
+  opts: {},
+  plugin: class extends Slatebox.node {
     doAttempt() {
-      console.log("HELLO NODE do I have inherited properties? ", this.disable);
+      console.log('HELLO NODE do I have inherited properties? ', this.disable);
     }
-  }
+  },
 });
